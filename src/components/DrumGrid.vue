95e83bb200f8ed9a2cac2d91d4d17ed45a20dcfa
bebari <rok.plevel@gmail.com>
2017-11-05 18:07:20 +0100

    - tracks initilazed with json file (parsed to Vue.js data) - implemented audio player

--- src/components/DrumGrid.vue
+++ src/components/DrumGrid.vue
@@ -2,10 +2,21 @@
   <div class="DrumGrid">
     <div class="grid-header">
       Number of steps: <input type="number" min="5" max="16" v-model="numSteps">
+      <button v-on:click="startPlayer">Play</button>
+      <button v-on:click="stopPlayer">Stop</button>
     </div>
     <div class="grid-main">
-      <div class="step" v-for="ix in parseInt(numSteps)" v-bind:key="ix">
-      <div v-for="sound in sounds" class="track" v-on:click="setTrackActive($event, sound.name)" v-bind:key="ix,sound" v-bind:class="sound.name"></div>
+      <div class="step" v-for="ix1 in parseInt(numSteps)" v-bind:key="ix1">
+        <template v-for="ix2 in tracks[ix1-1].sounds.length">
+          <div class="track" 
+              v-on:click="setTrackActive($event, tracks[ix1-1].sounds[ix2-1].name, ix1-1, ix2-1)" 
+              v-bind:key="ix1,ix2" 
+              v-bind:class="[tracks[ix1-1].sounds[ix2-1].name, {'active': tracks[ix1-1].sounds[ix2-1].active}]"></div>
+          <div class="player" 
+               v-bind:class="{'active': tracks[ix1-1].active}" 
+               v-if="ix2 < tracks[ix1-1].sounds.length" 
+               v-bind:key="ix2"></div>
+        </template>
       </div>
     </div>
   </div>
@@ -13,33 +24,65 @@
 </template>
 
 <script>
-//import VueHowler from 'vue-howler'
 
 export default {
   name: 'DrumGrid',
   //mixins: [VueHowler],
   data () {
     return {
-      numSteps: 5,
-      sounds: [
-        { name:'Clave01' },
-        { name:'Clave02' },
-        { name:'Cowbell' },
-        { name:'Kick01' },
-        { name:'Rim01' },
-        { name:'Rim02' },
-        { name:'Snare01' },
-        { name:'Snare02' }
-      ],
+      numSteps: 10,
+      tracks: []
     }
   },
   methods: {
-    setTrackActive: function(event, soundName) {
-        event.target.classList.toggle("active");
+    setTrackActive: function(event, soundName, ix1, ix2) {
+        //event.target.classList.toggle("active");
+        var isActive = this.tracks[ix1].sounds[ix2].active;
+        this.tracks[ix1].sounds[ix2].active = !isActive;
+
         var audio = new Audio("./static/sounds/" + soundName + ".wav");
+        audio.volume = 0.8;
         audio.play();
         //this.$els.audio.play();
+    },
+    updatePlayer: function () {},
+    startPlayer: function () {
+
+      var counter = 0;
+      var self = this;
+      this.stopPlayer();
+      this.updatePlayer = setInterval(function () {
+          if (counter > self.numSteps) {
+            counter = 0;
+          } else {
+            var sounds = self.tracks[counter].sounds;
+            for (var i = 0; i < sounds.length; i++) {
+              if (sounds[i].active) {
+                var audio = new Audio("./static/sounds/" + sounds[i].name + ".wav");
+                audio.volume = 0.8;
+                audio.play();
+              }
+            }
+
+            self.tracks[counter].active = true;
+            if (counter != 0) {
+              self.tracks[counter-1].active = false;
+            }
+            counter++;
+          }
+      }, 200);
+    },
+    stopPlayer: function () {
+      clearInterval(this.updatePlayer);
+      for (var i = 0; i < this.tracks.length; i++) {
+        this.tracks[i].active = false;
+      }
     }
+  },
+  created: function() {
+    this.$http.get('/static/data/tracks.json').then(function (response) {
+      this.tracks = response.data;
+    });
   }
 }
 </script>
@@ -70,14 +113,28 @@ export default {
     border: 5px solid black;
     width: 40px;
     height: 40px;
-    margin:5px;
+    margin:0px 5px;
     display:inline-block;
     background-color:#202224;
 }
 .track.active {
   background-color:#525252;
 }
-
+.player {
+  border: 2px solid green;
+  -webkit-transition: margin 0.1s ease-out;
+  -moz-transition: margin 0.1s ease-out;
+  -o-transition: margin 0.1s ease-out;
+  transition: margin 0.1s ease-out;
+  margin: 0px 26px 5px 26px;
+}
+.player.active{
+  -webkit-transition: margin 0.1s ease-out;
+  -moz-transition: margin 0.1s ease-out;
+  -o-transition: margin 0.1s ease-out;
+  transition: margin 0.1s ease-out;
+  margin:0px 22px 5px 22px;
+}
 .step {
   width:60px;
   display:inline-block;
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueResource from 'vue-resource'
 import DrumGrid from '@/components/DrumGrid'
 
 Vue.use(Router)
+Vue.use(VueResource);
 
 export default new Router({
   routes: [
